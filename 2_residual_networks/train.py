5e2cc77061fbd1e4f75116bc13fe95b22b8844a6
sarveshjnikas <sarveshjnikas1@gmail.com>
2026-09-22 22:12:37 +0530

    Add ResNet-18 architecture

--- 2_residual_networks/train.py
+++ 2_residual_networks/train.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
@@ -94,3 +95,83 @@ te_loader = DataLoader(te_dataset, batch_size=BATCH_SIZE, shuffle=False, num_wor
 
 tr_deter_dataset = Food101Dataset(tr_image_paths, test_transforms) # USED FOR FINAL EVALUATION ON TRAINING SET. NO DATA AUGMENTATION.
 tr_deter_loader = DataLoader(tr_deter_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=6, pin_memory=True)
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 =  nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU()
+
+        # DOWNSAMPLING.
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.downsample = None
+
+    def forward(self, x):
+        identity = x
+        conv1_out = self.conv1(x)
+        bn1_out = self.bn1(conv1_out)
+        relu1_out = self.relu1(bn1_out)
+        conv2_out = self.conv2(relu1_out)
+        bn2_out = self.bn2(conv2_out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        return self.relu2(bn2_out + identity)
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(ResNet18, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.body = nn.Sequential(
+            ResNetBlock(64, 64, stride=1),
+            ResNetBlock(64, 64, stride=1),
+
+            ResNetBlock(64, 128, stride=2),
+            ResNetBlock(128, 128, stride=1),
+
+            ResNetBlock(128, 256, stride=2),
+            ResNetBlock(256, 256, stride=1),
+
+            ResNetBlock(256, 512, stride=2),
+            ResNetBlock(512, 512, stride=1)
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512, 101)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.maxpool(x)
+        x = self.body(x)
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+    def loss(self, y_pred, y_true):
+        max_logits = y_pred.max(dim=1, keepdim=True).values
+        stabilized = y_pred - max_logits # FOR NUMERICAL SAFETY
+
+        log_sum_exp = torch.log(torch.exp(stabilized).sum(dim=1, keepdim=True))
+        log_probs = stabilized - log_sum_exp
+
+        correct_class_log_probs = log_probs.gather(1, y_true.unsqueeze(1)).squeeze(1)
+        return -correct_class_log_probs.mean()
